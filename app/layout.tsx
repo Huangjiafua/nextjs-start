a5f7eb286c6ac0413cc84ad2420966e91bb738ae
huangjiafu <1207134137@qq.com>
2025-03-15 14:03:22 +0800

    feat: init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
+import { inter } from "@/app/ui/fonts";
+import "@/app/ui/global.css";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  console.log(inter);
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
